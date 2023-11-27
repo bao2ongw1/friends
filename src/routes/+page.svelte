3285acba02d2e064bbdcf668fe17353411ee35c7
Esonhugh <esonhughoutside@gmail.com>
2023-11-27 15:34:40 +0800

    update description of header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,10 @@
 </h1>
 <p class="text-blue-500 text-center">
     This is Esonhugh Blog Friendlink, Visit this
-    <a alt="Github" href="https://github.com/Esonhugh/Friendlink_rebuild">Github repo</a>
+    <a class="text-blue-100" href="https://github.com/Esonhugh/friends">Github repo</a>
     to Add your Friend link with PR.
+	<br />
+	Main Blog: <a class="text-blue-100" href="https://eson.ninja"> eson.ninja </a>
 </p>
 <div class="p-3">
         <div class="flex-1 gap-3 grid md:grid-cols-3 ">
