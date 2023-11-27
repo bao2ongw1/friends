a7d41bbfe6d2c21023721d760e1381406928474f
Esonhugh <esonhughoutside@gmail.com>
2023-11-27 15:04:55 +0800

    init project

---
+++ src/lib/PersonalCard.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import type {PersonalData} from "$lib/PersonalData";
+    export let pd: PersonalData;
+    import {Avatar} from '@skeletonlabs/skeleton';
+
+    function JumpTo(url:string) {
+        window.open(url, "_blank")
+    }
+</script>
+
+<figure class="flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800" 
+    on:click={() => JumpTo(pd.url)}
+>
+    <div class="p-4">
+        <Avatar alt="{pd.alias}"
+        background="bg-surface-500 self-center"
+        hover={false} initials="{pd.alias}"
+        outlined={true} size="xl"
+        src="{pd.avatar}"
+        />
+    </div>
+    <div class="pt-6 text-center md:text-left space-y-4">
+      <blockquote>
+        <p class="text-lg font-medium p-2">
+            "{pd.remark}"
+        </p>
+      </blockquote>
+        <div class="text-sky-500 dark:text-sky-400 text-center p-1">
+          —— {pd.alias}
+        </div>
+    </div>
+  </figure>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import PersonalCard from "$lib/PersonalCard.svelte"
+	import { GlobalPool } from "$lib/PersonalData"
+</script>
+
+<h1 class="font-black text-4xl text-center text-blue-700 p-2">
+    Esonhugh Blog FriendLink
+</h1>
+<p class="text-blue-500 text-center">
+    This is Esonhugh Blog Friendlink, Visit this
+    <a alt="Github" href="https://github.com/Esonhugh/Friendlink_rebuild">Github repo</a>
+    to Add your Friend link with PR.
+</p>
+<div class="p-3">
+        <div class="flex-1 gap-3 grid md:grid-cols-3 ">
+            {#each GlobalPool as pd}
+                <PersonalCard {pd}/>
+            {/each}
+        </div>
+</div>
+
+<footer>
+    <p class="text-center p-2">
+        Esonhugh - 2022
+    </p>
+</footer>
