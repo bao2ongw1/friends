0429e17813725588427d887656b6ce30a39e69ec
baozongwi <146065986+bzq05@users.noreply.github.com>
2025-04-29 11:44:12 +0800

    Add baozongwi's friend link

--- src/lib/PersonalData.ts
+++ src/lib/PersonalData.ts
@@ -8,172 +8,178 @@ export interface PersonalData {
 
 export let GlobalPool :PersonalData[] = [
     {
-      "alias": "BryanMcNulty",
-      "url": "https://bryanmcnulty.com",
-      "avatar": "https://cdn.discordapp.com/avatars/844241499859451924/1efaeff42570664c4c551ac9153481da.webp",
-      "remark": "Bryan's Blog"
+        "alias": "BryanMcNulty",
+        "url": "https://bryanmcnulty.com",
+        "avatar": "https://cdn.discordapp.com/avatars/844241499859451924/1efaeff42570664c4c551ac9153481da.webp",
+        "remark": "Bryan's Blog"
     },
     {
-      "alias": "weepingdogel",
-      "url": "https://weepingdogel.github.io/",
-      "avatar": "https://weepingdogel.github.io/img/photo_2021-02-15_20-22-35.png",
-      "remark": "Once lost thing will never be able to come back."
+        "alias": "weepingdogel",
+        "url": "https://weepingdogel.github.io/",
+        "avatar": "https://weepingdogel.github.io/img/photo_2021-02-15_20-22-35.png",
+        "remark": "Once lost thing will never be able to come back."
     },
     {
-      "alias": "ajin",
-      "url": "https://ajin.cloud/",
-      "avatar": "https://avatars.githubusercontent.com/u/41041690?u=ceedc26d9b418d66d6dbdb0774e559347897cdbd&v=4",
-      "remark": "是世界上最好的人啊"
+        "alias": "ajin",
+        "url": "https://ajin.cloud/",
+        "avatar": "https://avatars.githubusercontent.com/u/41041690?u=ceedc26d9b418d66d6dbdb0774e559347897cdbd&v=4",
+        "remark": "是世界上最好的人啊"
     },
     {
-      "alias": "欠陥電気",
-      "url": "https://blog.atri.tk/",
-      "avatar": "https://blog.atri.tk/img/default_avatar.webp",
-      "remark": "划水摸鱼最快乐啦～"
+        "alias": "欠陥電気",
+        "url": "https://blog.atri.tk/",
+        "avatar": "https://blog.atri.tk/img/default_avatar.webp",
+        "remark": "划水摸鱼最快乐啦～"
     },
     {
-      "alias": "CRH6F-A-0464",
-      "url": "https://hydropwr.ca/",
-      "avatar": "https://hydropwr.ca/images/avatar.jpg",
-      "remark": "10th grade pseudo-geek/OIer/Railway Fan/ACG Rookie/μ’sic forever!"
+        "alias": "CRH6F-A-0464",
+        "url": "https://hydropwr.ca/",
+        "avatar": "https://hydropwr.ca/images/avatar.jpg",
+        "remark": "10th grade pseudo-geek/OIer/Railway Fan/ACG Rookie/μ’sic forever!"
     },
     {
-      "alias": "atsud0",
-      "url": "https://atsud0.me/",
-      "avatar": "https://images.atsud0.me/images/atsud0-avator.jpg",
-      "remark": "Procrastination is the thief of time."
+        "alias": "atsud0",
+        "url": "https://atsud0.me/",
+        "avatar": "https://images.atsud0.me/images/atsud0-avator.jpg",
+        "remark": "Procrastination is the thief of time."
     },
     {
-      "alias": "LeiWang",
-      "url": "https://leiblog.wang/",
-      "avatar": "https://leiblog.wang/images/avatar_2.jpg",
-      "remark": "LeiBlog"
+        "alias": "LeiWang",
+        "url": "https://leiblog.wang/",
+        "avatar": "https://leiblog.wang/images/avatar_2.jpg",
+        "remark": "LeiBlog"
     },
     {
-      "alias": "isabellainori",
-      "url": "https://justtolove.ml/",
-      "avatar": "http://justtolove.ml/images/3.jpeg",
-      "remark": "不定期跟新个人碎碎念&学习笔记,欢迎浏览!"
+        "alias": "isabellainori",
+        "url": "https://justtolove.ml/",
+        "avatar": "http://justtolove.ml/images/3.jpeg",
+        "remark": "不定期跟新个人碎碎念&学习笔记,欢迎浏览!"
     },
     {
-      "alias": "miaotony",
-      "url": "https://miaotony.xyz/",
-      "avatar": "https://cdn.jsdelivr.net/gh/miaotony/miaotony.github.io@master/medias/avatar.jpg",
-      "remark": "MeoW"
+        "alias": "miaotony",
+        "url": "https://miaotony.xyz/",
+        "avatar": "https://cdn.jsdelivr.net/gh/miaotony/miaotony.github.io@master/medias/avatar.jpg",
+        "remark": "MeoW"
     },
     {
-      "alias": "chuj",
-      "url": "https://www.cjovi.icu/",
-      "avatar": "https://www.cjovi.icu/usr/uploads/fixed/headpicture.png",
-      "remark": "不好好学习，生活中就会多出许多魔法和奇迹"
+        "alias": "chuj",
+        "url": "https://www.cjovi.icu/",
+        "avatar": "https://www.cjovi.icu/usr/uploads/fixed/headpicture.png",
+        "remark": "不好好学习，生活中就会多出许多魔法和奇迹"
     },
     {
-      "alias": "Atom",
-      "url": "https://homeboyc.cn/",
-      "avatar": "https://homeboyc.cn/img/avatar.jpg",
-      "remark": "生命不息 折腾不止"
+        "alias": "Atom",
+        "url": "https://homeboyc.cn/",
+        "avatar": "https://homeboyc.cn/img/avatar.jpg",
+        "remark": "生命不息 折腾不止"
     },
     {
-      "alias": "Gigalo",
-      "url": "https://www.waysoahc.xyz/",
-      "avatar": "http://q.qlogo.cn/headimg_dl?dst_uin=3163083993&spec=640&img_type=jpg",
-      "remark": "escape sky"
+        "alias": "Gigalo",
+        "url": "https://www.waysoahc.xyz/",
+        "avatar": "http://q.qlogo.cn/headimg_dl?dst_uin=3163083993&spec=640&img_type=jpg",
+        "remark": "escape sky"
     },
     {
-      "alias": "HeannaSumire",
-      "url": "https://galaxy-r.github.io/",
-      "avatar": "https://i.loli.net/2021/10/07/h67q2tOAVcSYEda.jpg",
-      "remark": "doyer?"
+        "alias": "HeannaSumire",
+        "url": "https://galaxy-r.github.io/",
+        "avatar": "https://i.loli.net/2021/10/07/h67q2tOAVcSYEda.jpg",
+        "remark": "doyer?"
     },
     {
-      "alias": "ek1ng",
-      "url": "https://ek1ng.com/",
-      "avatar": "https://ek1ng.com/img/avatar.jpg",
-      "remark": "ek1ng vidar 的师傅"
+        "alias": "ek1ng",
+        "url": "https://ek1ng.com/",
+        "avatar": "https://ek1ng.com/img/avatar.jpg",
+        "remark": "ek1ng vidar 的师傅"
     },
     {
-      "alias": "9Bie",
-      "url": "https://9bie.org/",
-      "avatar": "https://avatars.githubusercontent.com/u/12439321",
-      "remark": "渗透大师 可爱的音le 999999999Bie"
+        "alias": "9Bie",
+        "url": "https://9bie.org/",
+        "avatar": "https://avatars.githubusercontent.com/u/12439321",
+        "remark": "渗透大师 可爱的音le 999999999Bie"
     },
     {
-      "alias": "Bookfish",
-      "url": "https://bkfish.github.io/",
-      "avatar": "https://bkfish.github.io/images/logo.png",
-      "remark": "亲爱滴书鱼队长"
+        "alias": "Bookfish",
+        "url": "https://bkfish.github.io/",
+        "avatar": "https://bkfish.github.io/images/logo.png",
+        "remark": "亲爱滴书鱼队长"
     },
     {
-      "alias": "l0tus",
-      "url": "https://l0tus.vip/",
-      "avatar": "https://l0tus.vip/img/diluc.JPG",
-      "remark": "杭电22外院之光"
+        "alias": "l0tus",
+        "url": "https://l0tus.vip/",
+        "avatar": "https://l0tus.vip/img/diluc.JPG",
+        "remark": "杭电22外院之光"
     },
     {
-      "alias": "psyq",
-      "url": "https://blog.psyqlk.space/",
-      "avatar": "https://thirdqq.qlogo.cn/g?b=sdk&k=vFCLPytxmpqlzpxG55pdyw&s=640",
-      "remark": "正在加载中……如果未显示，请稍后刷新页面"
+        "alias": "psyq",
+        "url": "https://blog.psyqlk.space/",
+        "avatar": "https://thirdqq.qlogo.cn/g?b=sdk&k=vFCLPytxmpqlzpxG55pdyw&s=640",
+        "remark": "正在加载中……如果未显示，请稍后刷新页面"
     },
     {
-      "alias": "小吴玉麒麟",
-      "url": "https://blog.mjclouds.com",
-      "avatar": "https://thirdqq.qlogo.cn/g?b=sdk&k=nnzEWNwxMS88jKYre5fOjg&s=640",
-      "remark": "寄寄寄寄寄寄寄寄寄寄寄寄寄寄寄！"
+        "alias": "小吴玉麒麟",
+        "url": "https://blog.mjclouds.com",
+        "avatar": "https://thirdqq.qlogo.cn/g?b=sdk&k=nnzEWNwxMS88jKYre5fOjg&s=640",
+        "remark": "寄寄寄寄寄寄寄寄寄寄寄寄寄寄寄！"
     },
     {
-      "alias": "冰屋小站",
-      "url": "https://blog.biduang.cn",
-      "avatar": "https://cdn.friendship.org.cn/LightPicture/2022/10/406559cff5ae46ea.jpg",
-      "remark": "我码写我心"
+        "alias": "冰屋小站",
+        "url": "https://blog.biduang.cn",
+        "avatar": "https://cdn.friendship.org.cn/LightPicture/2022/10/406559cff5ae46ea.jpg",
+        "remark": "我码写我心"
     },
     {
-      "alias": "NX",
-      "url": "https://www.nickxu.top/",
-      "avatar": "https://www.nickxu.top/static/favicon.jpg",
-      "remark": "这家伙真勤奋，什么都留下了"
+        "alias": "NX",
+        "url": "https://www.nickxu.top/",
+        "avatar": "https://www.nickxu.top/static/favicon.jpg",
+        "remark": "这家伙真勤奋，什么都留下了"
     },
     {
-      "alias": "Magnesium",
-      "url": "https://cmmyyyy.github.io/",
-      "avatar": "https://thirdqq.qlogo.cn/g?b=sdk&k=ib70Wkgiawd1SD5gVrLcfZbw&kti=Y6-vYwAAAAI&s=640",
-      "remark": "Halo word?"
+        "alias": "Magnesium",
+        "url": "https://cmmyyyy.github.io/",
+        "avatar": "https://thirdqq.qlogo.cn/g?b=sdk&k=ib70Wkgiawd1SD5gVrLcfZbw&kti=Y6-vYwAAAAI&s=640",
+        "remark": "Halo word?"
     },
     {
-      "alias": "H3arn",
-      "url": "https://h3a.moe/",
-      "avatar": "https://cdn.jsdelivr.net/gh/h3a-moe/h3a-moe-asset/img/myavatar.jpg",
-      "remark": "NULL"
+        "alias": "H3arn",
+        "url": "https://h3a.moe/",
+        "avatar": "https://cdn.jsdelivr.net/gh/h3a-moe/h3a-moe-asset/img/myavatar.jpg",
+        "remark": "NULL"
     },
     {
-      "alias": "飞鸟记",
-      "url": "https://blog.aflybird.cn/",
-      "avatar": "https://avatars.githubusercontent.com/u/36830265",
-      "remark": "只能说真的是一只很爱飞的鸟吧"
+        "alias": "飞鸟记",
+        "url": "https://blog.aflybird.cn/",
+        "avatar": "https://avatars.githubusercontent.com/u/36830265",
+        "remark": "只能说真的是一只很爱飞的鸟吧"
     },
     {
-      "alias": "金羊的部落阁",
-      "url": "https://www.glodensheep.top/",
-      "avatar": "https://thirdqq.qlogo.cn/g?b=sdk&k=2jicu2tsxib3zaszMGRYcFYg&kti=ZEapnAAAAAE&s=640",
-      "remark": "The mind is its own place and, in itself can make a heaven of hell or a hell of heaven."
+        "alias": "金羊的部落阁",
+        "url": "https://www.glodensheep.top/",
+        "avatar": "https://thirdqq.qlogo.cn/g?b=sdk&k=2jicu2tsxib3zaszMGRYcFYg&kti=ZEapnAAAAAE&s=640",
+        "remark": "The mind is its own place and, in itself can make a heaven of hell or a hell of heaven."
     },
     {
-      "alias": "Glzjin",
-      "url": "https://www.zhaoj.in/",
-      "avatar": "https://www.zhaoj.in/wp-content/uploads/2016/04/1460635478e753758d45e5fb95f465e8ceaaabe897.png",
-      "remark": "信息安全爱好者"
+        "alias": "baozongwi",
+        "url": "https://baozongwi.xyz/",
+        "avatar": "https://baozongwi.xyz/images/acvtar.jpg",
+        "remark": "天地不仁，以万物为刍狗"
     },
     {
-      "alias": "M1n9K1n9",
-      "url": "https://blog.apt250.zip/",
-      "avatar": "https://blog.apt250.zip/wp-content/uploads/2023/08/ae772f0f-3eb9-2a16-62e4-c093f48b8b3b-300x300.png",
-      "remark": "信息安全爱好者"
+        "alias": "Glzjin",
+        "url": "https://www.zhaoj.in/",
+        "avatar": "https://www.zhaoj.in/wp-content/uploads/2016/04/1460635478e753758d45e5fb95f465e8ceaaabe897.png",
+        "remark": "信息安全爱好者"
     },
     {
-      "alias": "tari",
-      "url": "https://tari.moe/",
-      "avatar": "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fb3b2453f-7b80-493b-a4f0-b825427e1921%2FIMG_0016.png?table=collection&id=b68928df-fdf1-4821-90f2-59a530d0e2e6&t=b68928df-fdf1-4821-90f2-59a530d0e2e6&width=800&cache=v2",
-      "remark": "信息安全爱好者"
+        "alias": "M1n9K1n9",
+        "url": "https://blog.apt250.zip/",
+        "avatar": "https://blog.apt250.zip/wp-content/uploads/2023/08/ae772f0f-3eb9-2a16-62e4-c093f48b8b3b-300x300.png",
+        "remark": "信息安全爱好者"
     },
-  ]
+    {
+        "alias": "tari",
+        "url": "https://tari.moe/",
+        "avatar": "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fb3b2453f-7b80-493b-a4f0-b825427e1921%2FIMG_0016.png?table=collection&id=b68928df-fdf1-4821-90f2-59a530d0e2e6&t=b68928df-fdf1-4821-90f2-59a530d0e2e6&width=800&cache=v2",
+        "remark": "信息安全爱好者"
+    },
+]
   
